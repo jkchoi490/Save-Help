e900576cb88f2c297c2aadd6e7900d81e4a5c273
jkchoi490 <jkchoi490@daum.net>
2025-09-29 12:00:13 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/save_help/Save_Help/SaveHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.save_help.Save_Help;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
