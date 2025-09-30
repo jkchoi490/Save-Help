a33e77c1e91ab98e36c5678711f0568ee395fcfb
jkchoi490 <jkchoi490@daum.net>
2025-09-30 12:04:05 +0900

    feat: User Entity, LoginRequestDto, SignUpRequestDto 생성

---
+++ src/main/java/com/save_help/Save_Help/user/entity/User.java
@@ -0,0 +1,53 @@
+package com.save_help.Save_Help.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    private String name;
+
+    @Column(length = 255, unique = true)
+    private String email;
+
+    @Column(length = 255)
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private String provider;
+
+    @Column(name = "provider_user_id", nullable = false, length = 255)
+    private String providerUserId;
+
+    @Column(name = "profile_image_url", length = 500)
+    private String profileImageUrl;
+
+    private String nickname;
+
+    private String gender;
+
+    private int age;
+
+}
